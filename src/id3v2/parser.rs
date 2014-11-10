bd73be2a1f78d39b8e7ae4fdc90bdf6884243966
Jason Thompson <jason@jthompson.ca>
2014-11-10 07:12:31 -0500

    Figuring out Parser interface

--- src/id3v2/parser.rs
+++ src/id3v2/parser.rs
@@ -1,44 +1,56 @@
-use std::io::{BufferedReader, File};
+use std::io::{BufferedReader, Reader, File};
 use id3v2::header;
 use id3v2::frame;
 
-pub  struct Parser {
+pub struct Parser {
+    pub reader: BufferedReader<File>,
     pub current_position: uint,
     pub length_of_data: uint
 }
 
 impl Parser {
-    pub fn new() -> Parser {
+    pub fn new(path: &Path, reader: &BufferedReader<File>) -> Parser {
+        let mut file =File::open(path).unwrap();
+        
         Parser {
+            reader: reader,
             current_position: 0u,
             length_of_data: 0u
         }
     }
-    
-    pub fn parse(&mut self, path: &Path) -> SongMetadata {
-        let mut reader = BufferedReader::new(File::open(path));
 
+    pub fn get_next_frame_header(&self) -> Option<frame::FrameHeader> {
+        let frame_header_vec = self.reader.read_exact(10).unwrap();
+        if frame_header_vec[0] as char != 'T' {
+            None
+        } else {
+            Some(frame::FrameHeader::new(frame_header_vec))
+        }
+    }
+    
+    pub fn parse(&mut self) -> SongMetadata {
         // parse header
-        let header_vec = reader.read_exact(10);
-        let header = header::Header::new(&header_vec.unwrap());
+        
+        let header = header::Header::new(&self.reader.read_exact(10).unwrap());
+
         let mut song = SongMetadata::new(header);
 
+        let mut frame_header: frame::FrameHeader;
+        
+        match self.get_next_frame_header(){
+            Some(ref h) => h,
+            None => &frame_header,
+        };
 
-        // iterate through frames
-        while self.current_position.to_uint().unwrap() <= self.length_of_data {
-            // read frame header
-            let frame_header_vec = reader.read_exact(10).unwrap();
-            let frame_header = frame::FrameHeader::new(frame_header_vec);
-            let header_size = frame_header.size.clone();
-            let frame_contents_vec = reader.read_exact(frame_header.size).unwrap();
+        //let header_size = frame_header.
+        let frame_contents_vec = self.reader.read_exact(frame_header.size).unwrap();
 
-            let frame = frame::Frame::new(frame_header, frame_contents_vec);
-            &song.frames.push(frame);
+        let frame = frame::Frame::new(frame_header, frame_contents_vec);
+        &song.frames.push(frame);
 
-            self.current_position = header_size + 10;
-        }
         song
     }
+ 
 }
 
 pub struct SongMetadata {
